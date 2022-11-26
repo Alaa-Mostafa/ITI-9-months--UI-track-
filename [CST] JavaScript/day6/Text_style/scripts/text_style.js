36849d337c9e00454b462570258c6e6432ef63d5
Alaa-Mostafa <alaa.m.mostafa.kamel@gmail.com>
2022-11-26 20:25:30 +0200

    text change
    Apply different styles over a paragraph according to checked radio button. Implement the required functions in an external .js file to let TextStyle.html works properly

---
+++ [CST] JavaScript/day6/Text_style/scripts/text_style.js
@@ -0,0 +1,72 @@
+
+// ===================================================== changing font
+
+function ChangeFont(font_name)
+{
+    let paragraph = document.getElementById("PAR");
+    paragraph.style.fontFamily = font_name;
+}
+
+// ===================================================== changing text align
+
+function ChangeAlign(text_align)
+{
+    let paragraph = document.getElementById("PAR");
+    paragraph.style.textAlign = text_align;
+}
+
+// ===================================================== changing line height
+
+function ChangeHeight(line_height)
+{
+    let paragraph = document.getElementById("PAR");
+    paragraph.style.lineHeight = line_height;
+}
+
+// ===================================================== changing letter spacing
+
+function ChangeLSpace(letter_spacing)
+{
+    let paragraph = document.getElementById("PAR");
+    paragraph.style.letterSpacing = letter_spacing;
+}
+
+// ===================================================== changing text indent
+
+function ChangeIndent(text_indent)
+{
+    let paragraph = document.getElementById("PAR");
+    paragraph.style.textIndent = text_indent;
+}
+
+// ===================================================== changing text transform
+
+function ChangeTransform(text_transform)
+{
+    let paragraph = document.getElementById("PAR");
+    paragraph.style.textTransform = text_transform;
+}
+
+// ===================================================== changing text decoration
+
+function ChangeDecorate(text_decoration)
+{
+    let paragraph = document.getElementById("PAR");
+    paragraph.style.textDecoration = text_decoration;
+}
+
+// ===================================================== changing text border
+
+function ChangeBorder(text_border)
+{
+    let paragraph = document.getElementById("PAR");
+    paragraph.style.borderStyle = text_border;
+}
+
+// ===================================================== changing border color
+
+function ChangeBorderColor(border_color)
+{
+    let paragraph = document.getElementById("PAR");
+    paragraph.style.borderColor = border_color;
+}
