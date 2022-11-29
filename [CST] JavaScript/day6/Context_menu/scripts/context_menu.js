877499049af4a2b6ff25c249147acb45df1b6ff9
Alaa-Mostafa <alaa.m.mostafa.kamel@gmail.com>
2022-11-29 20:22:09 +0200

    prevent context menu on right click

---
+++ [CST] JavaScript/day6/Context_menu/scripts/context_menu.js
@@ -0,0 +1,9 @@
+
+/*
+    Prevent context menu from being displayed by preventing from mouse right click from its action. 
+    Note: context menu is the menu displayed when mouse right button is clicked
+*/
+
+document.addEventListener("contextmenu", function() {
+    event.preventDefault();
+});
